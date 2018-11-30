98edc0a3c2900284ef88cc2a353a6f858a1377f9
Dylanr57 <dylanr574423@gmail.com>
2018-11-29 16:01:40 -0800

    finished adding comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,19 @@ $(document).ready(function() {
     var total = 0;
     var duplicate = true;
 
+    // this function creates a random number between min and max
     function randomNumber(min,max) {
         return Math.floor(Math.random()*(max - min + 1) + min);
     }
 
+    // this function gives each of the 4 crystals a random value between 1 and 12
     function makeCrystals() {
         for ( i=0; i < crystals.length; i++) {
             crystals[i] = randomNumber(1,12);
         }
     }
 
+    // this function compares the array crystals to itself to check for duplicates, and return true or false depending on what it finds
     function checkDuplicate (array) {
         for (i=0; i <= array.length; i++) {
             for (j=0; j <= array.length; j++) {
@@ -28,6 +31,7 @@ $(document).ready(function() {
         return false;
     }
 
+    // This function resets the game and generates new numbers for the target and the crystals
     function reset () {
         target = randomNumber(19,120);
         duplicate = true;
@@ -39,6 +43,7 @@ $(document).ready(function() {
         console.log(crystals);
     }
 
+    // this function makes the crystals appear to blink by reducing opacity then returning it to normal opacity
     function blink (id) {
         $(id).animate({
             opacity: "0.5"
@@ -48,9 +53,11 @@ $(document).ready(function() {
         });
     }
 
+    // target is set to a random nmber between 19 and 120
     target = randomNumber(19,120);
     console.log(target)
 
+    // this runs a while loop that will remake the numbers in the crystals array until there are no duplicates
     while (duplicate === true) {
         makeCrystals();
         duplicate = checkDuplicate(crystals);
@@ -59,21 +66,26 @@ $(document).ready(function() {
     console.log(crystals);
 
     alert("Click on a crystal to start the game!")
-
+    
+    // When a crystal is clocked on...
     $(".crystals").on("click", function(){
 
+        // The target number is displayed
         $("#randomNumberText").html(target);
 
+        // This if statement runs if the user matches their total score to the target number, which increases their win count runs the reset function
         if ( total === target) {
             wins++;
             reset();
             alert("You win! Click on a crystal to play again!");
         }
+        // If the user's score is greater than the target, the number of losses increases and the reset function is run
         else if ( total > target) {
             losses++;
             reset();
             alert("You lose! Click on a crystal to play again!");
         }
+        // otherwise the program figures out which crystal was clicked on and runs the appropriate lines of code, which increase the total score by the vale assigned to the crystal clicked from the array crystals, and runs the blink function on the clicked crystal.
         else {
             if ($(this).is("#crystalOne")) {
                 total += crystals[0];  
@@ -93,6 +105,7 @@ $(document).ready(function() {
             }
             console.log(total);
         }
+        
         $("#totalScoreText").html(total);
         $("#winText").html(wins);
         $("#loseText").html(losses);
